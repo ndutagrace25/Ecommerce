58c015ab9c4e0e6fa667fa7719582809de41be5b
Grace Nduta <grace.nduta@mawingunetworks.com>
2020-07-04 20:58:57 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,22 @@
+// First slider
+$('.slider-one')
+    .not(".slick-initialized")
+    .slick({
+        autoplay: true,
+        autoplaySpeed: 3000,
+        dots: true,
+        prevArrow: ".site-slider .slider-btn .prev",
+        nextArrow: ".site-slider .slider-btn .next",
+
+    });
+
+// Second slider
+$('.slider-two')
+    .not(".slick-initialized")
+    .slick({
+        prevArrow: ".site-slider-two .prev",
+        nextArrow: ".site-slider-two .next",
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        autoplaySpeed: 3000
+    });
